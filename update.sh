72fae09dd6dbc7bfbacb2f516250cf8b228c5c71
Spencer Bliven <spencer.bliven@gmail.com>
2024-03-07 09:48:43 +0100

    Add blank levels for use as templates
    Levels are in the standard horizontal net with 2, 4, or 8 tiles
    between pentagons.

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 # Update all levels
 
 for level in levels/*.yaml; do
+    echo "Processing $(basename "${level%.*}")"
     out="docs/maps/$(basename "${level%.*}")"
     python mlu_map.py "$level" "${out}.svg"
     magick convert "${out}.svg" "${out}.png"
